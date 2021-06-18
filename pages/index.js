95c5d72a5e809d9a36951ae283a3fd3e19a0cb17
unknown <f.geanlopes@gmail.com>
2021-06-18 01:05:24 -0300

    adicionado validação e input do whatsapp

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
   const [habilitaBtn, setHabilitaBtn] = useState(false);
   const [mensagemBtn, setMensagemBtn] = useState('Digite o número acima');
 
-  const clearInput = useCallback(()=>{
-    // setTimeout(setNumero(''), 1000);
+  const callMessage = useCallback(()=>{
+    
   },[]);
 
   const handleScroll = useCallback((element)=>{
@@ -21,14 +21,36 @@ export default function Home() {
   const handleInputChange = useCallback((e)=>{
     let dig = e.target.value;
     
-    dig = dig.replace(/\D/g,"");                   //Remove tudo o que não é dígito
-    dig = dig.replace(/^(\d{2})(\d)/g,"($1) $2");  //Coloca parênteses em volta dos dois primeiros dígitos
-    dig = dig.replace(/(\d)(\d{4})$/,"$1-$2");     //Coloca hífen entre o quarto e o quinto dígitos
+    dig = dig.replace(/\D/g,"")
+    dig = dig.replace(/^(\d{2})(\d)/g,"($1) $2")
+    dig = dig.replace(/(\d)(\d{4})$/,"$1-$2")
 
+    dig.length === 0 && setMensagemBtn('Digite o número acima')
+    dig.length > 0 && setMensagemBtn('Digite número completo')
     setNumero(dig)
-
   },[numero]);
 
+  useEffect(()=>{
+    let checkValidate = document.getElementById("check_number");
+    let animate = document.querySelector(".button_wp");
+
+    if(numero.length === 15){
+      checkValidate.classList.add("check");
+      checkValidate.classList.remove("none");
+      setMensagemBtn("Chamar número")
+      setHabilitaBtn(true);
+    }else{
+      checkValidate.classList.remove("check");
+      checkValidate.classList.add("none");
+      setHabilitaBtn(false);
+    }
+    
+    animate.classList.add("animate");
+    setTimeout(() => {
+      animate.classList.remove("animate")
+    }, 100);
+        
+  },[numero])
 
   return (
     <>
@@ -41,19 +63,26 @@ export default function Home() {
             {/* COLOCAR TECLADO DE NUMERO DO CELULAR */}
             <div className="link_saiba_mais button" onClick={()=>{handleScroll('project')}}>Conheça o projeto</div>
 
-            <input 
-              type="text"
-              value={numero}
-              maxLength="15"
-              placeholder="Digite número aqui"
-              onChange={(e)=>{handleInputChange(e)}}
-            />
+            <div className="input_number">
+              <input 
+                type="tel"
+                value={numero}
+                maxLength="15"
+                placeholder="Digite número aqui"
+                onChange={(e)=>{handleInputChange(e)}}
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" id="check_number" class="none" width="20" height="18" viewBox="0 0 166 150.9"><path d="M0.3 96l62.4 54.1L165.6 0.3"/></svg>
+            </div>
 
             {habilitaBtn ? 
-                <a onClick={clearInput} target="_blank" className="button btn_chamar" 
-                    href={`https://api.whatsapp.com/send?phone=55${numero}`}/>
+                <a className="button_wp button btn_chamar bold" onClick={callMessage} target="_blank" href={`https://api.whatsapp.com/send?phone=55${numero}`}>
+                    <p>{mensagemBtn}</p>
+                    <svg width="18" height="12" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M14.1377 5.33853C14.4002 5.0734 14.3942 4.64943 14.1243 4.39157L9.72539 0.189606C9.45548 -0.0682511 9.02379 -0.0623632 8.76125 0.202757C8.4987 0.467873 8.5047 0.891829 8.77466 1.14968L11.9485 4.18149C12.353 4.56784 12.3619 5.21081 11.9684 5.60825L8.88179 8.7251C8.61925 8.99023 8.62525 9.41416 8.89516 9.67202C9.16511 9.92987 9.59675 9.92398 9.85929 9.65889L14.1377 5.33853ZM0.00934568 5.05802C0.0145731 5.4277 0.318397 5.7232 0.688077 5.71815L12.9885 5.55035C13.3584 5.5453 13.6542 5.24124 13.6489 4.87131C13.6437 4.50164 13.3399 4.20615 12.9702 4.21119L0.66981 4.37895C0.299868 4.384 0.00411458 4.68808 0.00934568 5.05802Z" fill="white"/>
+                    </svg>
+                </a>
                 : 
-                <div className="button btn_chamar">
+                <div className="button_wp button btn_chamar">
                     <p>{mensagemBtn}</p>
                 </div>
             }
