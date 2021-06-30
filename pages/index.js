1792be336ec09e2e64e3a305ce8af115646afb5e
unknown <f.geanlopes@gmail.com>
2021-06-30 00:52:02 -0300

    adicionado metas seo

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
-import react, {useState, useCallback, useEffect} from 'react';
+import {useState, useCallback, useEffect} from 'react';
+import Head from 'next/head';
 import Image from 'next/image';
-import axios from 'axios';
 
 export default function Home() {
   
-  const [numero, setNumero] = useState('');
+  const [values, setValues] = useState('');
+  const [callWhatsapp, setCallWhatsapp] = useState('');
   const [randomColor, setRandoColor] = useState("");
-  const [statusCheckNumber, setStatusCheckNumber] = useState("Validar");
   const [habilitaBtn, setHabilitaBtn] = useState(false);
   const [mensagemBtn, setMensagemBtn] = useState('Digite o número acima');
 
   // GENERATION COLORS
   useEffect(()=>{
     setRandoColor(Math.floor(Math.random()*16777215).toString(16.6));
-      let aaa = {"validacao": "" , "erro": "\n\t  Error! Unknown error. Please try again.\n\t"};
-      // console.log(aaa.erro);
-  },[])
+    window.onscroll = () => {
+      let to = window.scrollY;
+      let up = document.querySelector(".toUp");
+      console.log(up);
+  
+      if (to > 100) up.classList.add("active");
+      else up.classList.remove("active");
+    };
+  },[]);
 
   // SCROLL ANIMATE
   const handleScroll = useCallback((element)=>{
@@ -24,7 +30,7 @@ export default function Home() {
     });
   },[]);
 
-  // ENTRADA DE DADO
+  // INPUT DATA ENTRY
   const handleInputChange = useCallback((e)=>{
     let dig = e.target.value;
     
@@ -34,34 +40,16 @@ export default function Home() {
 
     dig.length === 0 && setMensagemBtn('Digite o número acima')
     dig.length > 0 && setMensagemBtn('Digite número completo')
-    setNumero(dig)
-  },[numero]);
+    
+    setValues(dig);
+  },[values]);
   
-  // CHECK REGISTERED WHATSAPP
-  const checkNumber = useCallback( async ()=>{
-    let number = numero.replace("-","").replace(" ", "").replace("(", "").replace(")", "");    
-    if(number.length === 11){
-      setStatusCheckNumber('Validando...');
-      let resultNumber = await axios.post(`https://polar-plains-38379.herokuapp.com/check?number=${number}`);
-      let [data] = resultNumber.data;
-
-      if(data.validacao === "Exists on WhatsApp!"){
-        setStatusCheckNumber('Número cadastrado!');
-      }
-      else if(data.validacao === "No exists on WhatsApp"){
-        setStatusCheckNumber('Número não cadastrado!');
-      }
-      else if(data.erro === "\n\t  Error! Unknown error. Please try again.\n\t"){
-        setStatusCheckNumber('Falha ao validar!');
-      }
-    }
-  },[numero])
-  
+  //ANIMATE INPUT
   useEffect(()=>{
     let checkValidate = document.getElementById("check_number");
     let animate = document.querySelector(".button_wp");
 
-    if(numero.length === 15){
+    if(values.length === 15){
       checkValidate.classList.add("check");
       checkValidate.classList.remove("none");
       setMensagemBtn("Chamar número")
@@ -76,47 +64,55 @@ export default function Home() {
     setTimeout(() => {
       animate.classList.remove("animate")
     }, 100);
-        
-  },[numero])
+
+  },[values]);
+
+  // REPLACE CALLWHATSAPP
+  useEffect(()=>{
+    setCallWhatsapp(values.replace("-","").replace(" ", "").replace("(", "").replace(")", ""));
+  },[values]);
 
   return (
     <>
+      <Head>
+          <title>Chama no zap | Seja mais produtivo</title>
+          <meta name="description" content="Chame seus contatos sem salvar no WhatsApp." />
+          <meta name="author" content="Gean Lopes | https://www.linkedin.com/in/geanlopes/"/>
+          <meta name="keywords" content="Gean Lopes,Chama no zap,Seja mais produtivo"/>
+          <meta property="og:title" content="Chama no zap" />
+          <meta property="og:image" content="/chamanozap_web.png" />
+
+          <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon/site.webmanifest"/>
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="theme-color" content="#ffffff"/>
+      </Head>
       <section className="home" id="home" style={{ backgroundImage: `linear-gradient(to bottom, ${`#`+ randomColor}, black)`}}>
           <div className="content">
             <div className="logo">
-              <Image src="/logo.png" alt="Ícone do projeto chama no zap" width="331px" height="191px"/>
+              <Image src="/logo.png" alt="Chama no zap" width="331px" height="191px"/>
             </div>
-
-            {/* COLOCAR TECLADO DE NUMERO DO CELULAR */}
             <div className="link_saiba_mais button" onClick={()=>{handleScroll('project')}}>Conheça o projeto</div>
-
             <div className="input_number">
-              <input 
-                type="tel"
-                value={numero}
-                maxLength="15"
-                placeholder="Digite número aqui"
-                onChange={(e)=>{handleInputChange(e)}}
-              />
+              <input type="tel" value={values} maxLength="15" placeholder="Digite número aqui" onChange={(e)=>{handleInputChange(e)}}/>
               <svg xmlns="http://www.w3.org/2000/svg" id="check_number" className="none" width="20" height="18" viewBox="0 0 166 150.9"><path d="M0.3 96l62.4 54.1L165.6 0.3"/></svg>
             </div>
-
-            <a className="button_wp button btn_chamar bold" onClick={()=>{checkNumber()}}>{statusCheckNumber}</a>
-
-            {/* {habilitaBtn ? 
-                <a className="button_wp button btn_chamar bold" onClick={callMessage} 
-                // target="_blank" href={`https://api.whatsapp.com/send?phone=55${numero}`}
-                >
-                    <p>{mensagemBtn}</p>
-                    <svg width="18" height="12" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M14.1377 5.33853C14.4002 5.0734 14.3942 4.64943 14.1243 4.39157L9.72539 0.189606C9.45548 -0.0682511 9.02379 -0.0623632 8.76125 0.202757C8.4987 0.467873 8.5047 0.891829 8.77466 1.14968L11.9485 4.18149C12.353 4.56784 12.3619 5.21081 11.9684 5.60825L8.88179 8.7251C8.61925 8.99023 8.62525 9.41416 8.89516 9.67202C9.16511 9.92987 9.59675 9.92398 9.85929 9.65889L14.1377 5.33853ZM0.00934568 5.05802C0.0145731 5.4277 0.318397 5.7232 0.688077 5.71815L12.9885 5.55035C13.3584 5.5453 13.6542 5.24124 13.6489 4.87131C13.6437 4.50164 13.3399 4.20615 12.9702 4.21119L0.66981 4.37895C0.299868 4.384 0.00411458 4.68808 0.00934568 5.05802Z" fill="white"/>
-                    </svg>
-                </a>
-                : 
-                <div className="button_wp button btn_chamar">
-                    <p>{mensagemBtn}</p>
-                </div>
-            } */}
+            {habilitaBtn ? 
+              <a className="button_wp button btn_chamar bold" 
+              target="_blank" href={`https://wa.me/55${callWhatsapp}`}>
+                <p>{mensagemBtn}</p>
+                <svg width="18" height="12" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14.1377 5.33853C14.4002 5.0734 14.3942 4.64943 14.1243 4.39157L9.72539 0.189606C9.45548 -0.0682511 9.02379 -0.0623632 8.76125 0.202757C8.4987 0.467873 8.5047 0.891829 8.77466 1.14968L11.9485 4.18149C12.353 4.56784 12.3619 5.21081 11.9684 5.60825L8.88179 8.7251C8.61925 8.99023 8.62525 9.41416 8.89516 9.67202C9.16511 9.92987 9.59675 9.92398 9.85929 9.65889L14.1377 5.33853ZM0.00934568 5.05802C0.0145731 5.4277 0.318397 5.7232 0.688077 5.71815L12.9885 5.55035C13.3584 5.5453 13.6542 5.24124 13.6489 4.87131C13.6437 4.50164 13.3399 4.20615 12.9702 4.21119L0.66981 4.37895C0.299868 4.384 0.00411458 4.68808 0.00934568 5.05802Z" fill="white"/>
+                </svg>
+              </a>
+            : 
+              <div className="button_wp button btn_chamar">
+                  <p>{mensagemBtn}</p>
+              </div>
+            }
           </div>
       </section>
       <section className="project" id="project">
@@ -127,9 +123,15 @@ export default function Home() {
       <section className="contact">
         <h2>Saiba Mais</h2>
         <div className="rede_social">
-          <Image src="/linkedin.png" alt="Ícone do Linkedin do Gean Lopes" width="45px" height="45px"/>
-          <Image src="/email.png" alt="Ícone do E-mail do Gean Lopes" width="45px" height="45px"/>
-          <Image src="/github.png" alt="Ícone do Github do Gean Lopes" width="45px" height="45px"/>
+          <a href="https://www.linkedin.com/in/geanlopes/" target="_blank">
+            <Image src="/linkedin.png" alt="Linkedin Gean Lopes" width="45px" height="45px"/>
+          </a>
+          <a href="mailto:f.geanlopes@gmail.com" target="_blank">
+            <Image src="/email.png" alt="E-mail Gean Lopes" width="45px" height="45px"/>
+          </a>
+          <a href="https://github.com/fgeanlopes" target="_blank">
+            <Image src="/github.png" alt="Github Gean Lopes" width="45px" height="45px"/>
+          </a>
         </div>
         <p className="version">V.3</p>
         <div className="effect_footer" style={{ backgroundImage: `linear-gradient(to top, ${`#`+ randomColor}, black)`}}></div>
@@ -144,11 +146,9 @@ export default function Home() {
             </div>
         </div>
       </a>
-
-      {/* <div className="voltarHome" onClick={handleScroll('home')}>
-        Subir
-      </div> */}
-      {/* <BotaoSubir/> */}
+      <div style={{ backgroundColor: `${`#`+ randomColor}`}} className="toUp" onClick={()=>{handleScroll('home')}}>
+        <Image src="/arrow.svg" width="30px" height="17px" />
+      </div>
     </>
   )
 }
