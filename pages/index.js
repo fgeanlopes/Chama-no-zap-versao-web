5e98986ece95c94cfd396a1778bbc9518db42786
Gean Lopes <43907478+fgeanlopes@users.noreply.github.com>
2021-07-13 23:13:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,22 +32,6 @@ export default function Home(props) {
     let acesso = {dia, hora:`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`}
 
     let firebaseConfig = {
-      // apiKey:process.env.APIKEY,
-      // authDomain:process.env.AUTHDOMAIN,
-      // projectId:process.env.PROJECTID,
-      // storageBucket:process.env.STORAGEBUCKET,
-      // messagingSenderId:process.env.MESSAGINGSENDERID,
-      // appId:process.env.APPID,
-      // measurementId:process.env.MEASUREMENTID
-      
-      // apiKey:process.env.NEXT_PUBLIC_APIKEY,
-      // authDomain:process.env.NEXT_PUBLIC_AUTHDOMAIN,
-      // projectId:process.env.NEXT_PUBLIC_PROJECTID,
-      // storageBucket:process.env.NEXT_PUBLIC_STORAGEBUCKET,
-      // messagingSenderId:process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-      // appId:process.env.NEXT_PUBLIC_APPID,
-      // measurementId:process.env.NEXT_PUBLIC_MEASUREMENTID
-
       apiKey: "AIzaSyCNi1WmPNkv2-amZP7ZoPCRKaCEqUIY03k",
       authDomain: "chama-no-zap-fb014.firebaseapp.com",
       projectId: "chama-no-zap-fb014",
